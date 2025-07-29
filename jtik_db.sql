c2ff4168dce8c5e4aec3cb91e0816d677ea91a71
Barajapu <142334729+23Barajapu@users.noreply.github.com>
2025-07-29 18:24:41 +0700

    Add files via upload

--- jtik_db.sql
+++ jtik_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 18, 2025 at 05:48 AM
+-- Generation Time: Jul 29, 2025 at 11:24 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.3.22
 
@@ -42,30 +42,6 @@ CREATE TABLE `beritas` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cache`
---
-
-CREATE TABLE `cache` (
-  `key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `value` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `expiration` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cache_locks`
---
-
-CREATE TABLE `cache_locks` (
-  `key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `owner` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `expiration` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `cpls`
 --
 
@@ -91,82 +67,42 @@ CREATE TABLE `dosens` (
   `foto` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `bidang_keahlian` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `prodi` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `dosens`
 --
 
-INSERT INTO `dosens` (`id`, `nama`, `nip`, `jabatan`, `foto`, `bidang_keahlian`, `created_at`, `updated_at`) VALUES
-(1, 'Nunu Nugraha Purnawan, S.Pd., M.Kom.', '197909152015041001', '', NULL, NULL, '2025-07-16 13:21:56', '2025-07-16 13:21:56'),
-(2, 'Rian Piarna, S.E., M.M.', '198709032019031009', '', NULL, NULL, '2025-07-16 13:21:56', '2025-07-16 13:21:56'),
-(3, 'Dwi Vernanda, S.T., M.Pd.', '199104302019032018', '', NULL, NULL, '2025-07-16 13:21:56', '2025-07-16 13:21:56'),
-(4, 'Mohammad Iqbal, S.Kom., M.T.', '199001262019031025', '', NULL, NULL, '2025-07-16 13:21:56', '2025-07-16 13:21:56'),
-(5, 'Tri Herdiawan Apandi, S.ST., M.T.', '198801052019031008', '', NULL, NULL, '2025-07-16 13:21:56', '2025-07-16 13:21:56'),
-(6, 'Slamet Rahayu, S.Pd., M.Pd.', '170900045', '', NULL, NULL, '2025-07-16 13:21:56', '2025-07-16 13:21:56'),
-(7, 'Haryati, S.Pd., M.Pd.', '199306142019032021', '', NULL, NULL, '2025-07-16 13:21:56', '2025-07-16 13:21:56'),
-(8, 'Nurfitria Khoirunnisa, S.Tr.Kom., M.Kom.', '199603112020122022', '', NULL, NULL, '2025-07-16 13:21:56', '2025-07-16 13:21:56'),
-(9, 'Sari Azhariyah, S.Pd., M.Pd.T.', '199408182022032017', '', NULL, NULL, '2025-07-16 13:21:56', '2025-07-16 13:21:56'),
-(10, 'Taufan Abdurrachman, M.Kom.', '199311112022031006', '', NULL, NULL, '2025-07-16 13:21:56', '2025-07-16 13:21:56'),
-(11, 'Chepy Perdana, M.Pd.', '199109242022031001', '', NULL, NULL, '2025-07-16 13:21:56', '2025-07-16 13:21:56'),
-(12, 'Lani Nurlani, M.Kom.', '198804032022032002', '', NULL, NULL, '2025-07-16 13:21:56', '2025-07-16 13:21:56'),
-(13, 'Masesa Angga Wijaya, S.Kom., M.Kom.', '198808132022031004', '', NULL, NULL, '2025-07-16 13:21:56', '2025-07-16 13:21:56'),
-(14, 'Usep Abdul Rosid, S.T., M.Kom.', '198905122022031003', '', NULL, NULL, '2025-07-16 13:21:56', '2025-07-16 13:21:56'),
-(15, 'Erick Febriyanto, S.Kom, M.T.I.', '199002212024061001', '', NULL, NULL, '2025-07-16 13:21:56', '2025-07-16 13:21:56'),
-(16, 'Abd Rachman Mildan, M.Pd.', '198910182024061001', '', NULL, NULL, '2025-07-16 13:21:56', '2025-07-16 13:21:56'),
-(17, 'Agin Sugiwa, M.Ak.', '198909022024061001', '', NULL, NULL, '2025-07-16 13:21:56', '2025-07-16 13:21:56');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `failed_jobs`
---
-
-CREATE TABLE `failed_jobs` (
-  `id` bigint UNSIGNED NOT NULL,
-  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `jobs`
---
-
-CREATE TABLE `jobs` (
-  `id` bigint UNSIGNED NOT NULL,
-  `queue` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `attempts` tinyint UNSIGNED NOT NULL,
-  `reserved_at` int UNSIGNED DEFAULT NULL,
-  `available_at` int UNSIGNED NOT NULL,
-  `created_at` int UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `job_batches`
---
-
-CREATE TABLE `job_batches` (
-  `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `total_jobs` int NOT NULL,
-  `pending_jobs` int NOT NULL,
-  `failed_jobs` int NOT NULL,
-  `failed_job_ids` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `options` mediumtext COLLATE utf8mb4_unicode_ci,
-  `cancelled_at` int DEFAULT NULL,
-  `created_at` int NOT NULL,
-  `finished_at` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO `dosens` (`id`, `nama`, `nip`, `jabatan`, `foto`, `bidang_keahlian`, `created_at`, `updated_at`, `prodi`) VALUES
+(1, 'Dwi Vernanda, S.T., M.Pd.', '199104302019032000', 'Ketua Jurusan', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'SI'),
+(2, 'Haryati, S.Pd., M.Pd.', '199306142019032000', 'Sekretaris Jurusan', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'TRPL'),
+(3, 'Lani Nurlani, S.T., M.Kom.', '198804032020032000', 'Kaprodi SI', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'SI'),
+(4, 'Nunu Nugraha P., S.Pd., M.Kom', '197909152015041001', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'SI'),
+(5, 'Rian Piarna, S.E., M.M.', '198709032019031009', 'Ka Lab MDI', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'SI'),
+(6, 'Slamet Rahayu, S.Pd., M.Pd.', '170900045', 'Ka Lab JARKOM', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'SI'),
+(7, 'Sari Azhariyah, S.Pd., M.Pd.T.', '199408182022032000', 'Ka Lab RPL', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'SI'),
+(8, 'Chepy Perdana, S.Kom., M.Pd', '199109242022031000', 'Ka Lab UX', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'SI'),
+(9, 'Usep Abdul Rosid, S.T., M.Kom.', '198905122022031000', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'SI'),
+(10, 'Masesa Angga Wijaya, S.Kom., M.Kom.', '198808132022031000', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'SI'),
+(11, 'Abd Rachman Mildan, M.Pd', '198910182024061000', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'SI'),
+(12, 'Rahmat Irsyada, M.Pd', '', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'SI'),
+(13, 'Mohammad Iqbal, S.Kom., M.T.', '199001262019031000', 'Kaprodi TRPL', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'TRPL'),
+(14, 'Taufan Abdurrachman, S.T., M.Kom.', '199311112022031000', 'Ka Lab SI', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'TRPL'),
+(15, 'Nurfitria Khoirunnisa, S.Tr.Kom., M.Kom.', '199603112020122000', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'TRPL'),
+(16, 'Tri Herdiawan A., S.ST., M.T.', '198801052019031000', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'TRPL'),
+(17, 'Erick Febriyanto, S.Kom., M.T.I.', '199002212024061000', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'TRPL'),
+(18, 'Willy Muhammad Fauzi, S.T., M.Kom.', '199004252022031000', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'TRPL'),
+(19, 'Agin Sugiwa M.Ak', '198909022024061000', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'Akuntansi'),
+(20, 'Arief budiman, M.Ak', '199107252022031000', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'Akuntansi'),
+(21, 'Liandy L Tobing, S.Pd., M.M.', '199104122022031000', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'TRPL'),
+(22, 'Ardhi Akmahudin Jadhira, S.Kom., M.Kom.', '199601132022056000', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'TRPL'),
+(23, 'Gilang Anka Dilang, S.Pd., M.M', '199601222022056000', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'TRPL'),
+(24, 'Rian Hermawan, S.Kom.,M.Kom', '199601222022056000', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'TRPL'),
+(25, 'Herdawati, S.E., M.Sc.', '199703222022031000', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'TRPL'),
+(26, 'Khaida Hamam, S.Pd., M.Ak.', '199703222022031000', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'TRPL'),
+(27, 'Muhammad Juwiansah Abdullah, S.Pd.,M.Pd', '199703222022031000', 'Dosen', NULL, NULL, '2025-07-29 03:04:04', '2025-07-29 03:04:04', 'TRPL');
 
 -- --------------------------------------------------------
 
@@ -387,19 +323,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (17, '2025_07_16_175048_create_tendiks_table', 1),
 (18, '2025_07_16_175049_create_beritas_table', 1),
 (19, '2025_07_16_175051_create_cpls_table', 2),
-(20, '2025_07_16_175052_create_mata_kuliahs_table', 3);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `password_reset_tokens`
---
-
-CREATE TABLE `password_reset_tokens` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+(20, '2025_07_16_175052_create_mata_kuliahs_table', 3),
+(21, '2025_07_29_100126_add_prodi_to_dosens_table', 4),
+(22, '2025_07_29_101655_add_nip_prodi_to_tendiks_table', 5);
 
 -- --------------------------------------------------------
 
@@ -445,7 +371,12 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('6w2IrNhiqTQPLH96xkqbloGCMcgAWrSSSfTgKk4y', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiTWtIbU4zeFpwd0ttQmpTeGxlVFFIYTU2V2d1bmVHNnpvVmVXRW8wWSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NDc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9pbWcvcGxhY2Vob2xkZXItbmV3czIucG5nIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1752817685);
+('2Sas2NLdABk4KYxzfOJdaH0MpyR5k0BOgqsdEgIf', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoickVVcTloQ01NUm43d2RsWEFkVUN6czFMTERyQkg4RUdpS0VUUWNXRCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1753788012),
+('2X3x4kjwWYpPEB93XCO4mikCzt0s6vP50n432nee', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoidWVWd1hUY1FMMmJTbkNxa3Rrd0JnVEVvV1NUTGxRRVdWRW1OeHE3RCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1753114883),
+('GIdpf5Dtc4Ao0bqsSh2Qn266qIuHYwz6jlWhxcTI', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoibnlJMVZBWUkzVmU4dVVFYzhaNHVjaVhONG5qazJoZFJ6V2hJbWJTMCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1753439481),
+('kuSKzFcA7zBRmHD4zGzugTWepubokFyZDu6jivCO', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiM1VDOVVzRVhJbDBoeU1iU1ZKYTh6MXIxM2ZhZ3g0U3k2amF1Q0t0cyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1753770367),
+('Vu8ODrEdUxzUDhWKimtfipS3nalcGjCAJRU6HXnz', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoidjFSUXZNdXFGUnhlcHlaRFYyalFNUlpCTFEzZkh5UlZ0VVVEV0FIeSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1753438914),
+('zXAYucAk7t7HyYBOZ2FIJDIdf0VJ8eSCqFWGcIdG', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiRWZlM0I4R0Z4amhnV1Z4YnViVk9iRHVGckJpS0w4N2tZYjlzSjNhSSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1752866828);
 
 -- --------------------------------------------------------
 
@@ -456,7 +387,9 @@ INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, 
 CREATE TABLE `tendiks` (
   `id` bigint UNSIGNED NOT NULL,
   `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nip` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `jabatan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prodi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `foto` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -466,9 +399,10 @@ CREATE TABLE `tendiks` (
 -- Dumping data for table `tendiks`
 --
 
-INSERT INTO `tendiks` (`id`, `nama`, `jabatan`, `foto`, `created_at`, `updated_at`) VALUES
-(1, 'Rina Dewi', 'Tendik', 'vl-team-1.4.png', '2025-07-16 13:21:56', '2025-07-16 13:21:56'),
-(2, 'Agus Santoso', 'Tendik', 'vl-team-1.5.png', '2025-07-16 13:21:56', '2025-07-16 13:21:56');
+INSERT INTO `tendiks` (`id`, `nama`, `nip`, `jabatan`, `prodi`, `foto`, `created_at`, `updated_at`) VALUES
+(1, 'Wulan Siti Nurul Masriah, S.E.', '198508192021212000', 'PLP', 'TENDIK', NULL, '2025-07-29 03:17:42', '2025-07-29 03:17:42'),
+(2, 'Erin Tri Rezqi Maudy, A.Md.Kom', '240100109', 'Teknisi', 'TENDIK', NULL, '2025-07-29 03:17:42', '2025-07-29 03:17:42'),
+(3, 'Cica Rismawanti, A.Md.Kom', '199603122025062000', 'Admin', 'TENDIK', NULL, '2025-07-29 03:17:42', '2025-07-29 03:17:42');
 
 -- --------------------------------------------------------
 
@@ -506,18 +440,6 @@ ALTER TABLE `beritas`
   ADD KEY `beritas_kategori_berita_id_foreign` (`kategori_berita_id`);
 
 --
--- Indexes for table `cache`
---
-ALTER TABLE `cache`
-  ADD PRIMARY KEY (`key`);
-
---
--- Indexes for table `cache_locks`
---
-ALTER TABLE `cache_locks`
-  ADD PRIMARY KEY (`key`);
-
---
 -- Indexes for table `cpls`
 --
 ALTER TABLE `cpls`
@@ -528,26 +450,6 @@ ALTER TABLE `cpls`
 -- Indexes for table `dosens`
 --
 ALTER TABLE `dosens`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
-
---
--- Indexes for table `jobs`
---
-ALTER TABLE `jobs`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `jobs_queue_index` (`queue`);
-
---
--- Indexes for table `job_batches`
---
-ALTER TABLE `job_batches`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -568,12 +470,6 @@ ALTER TABLE `mata_kuliahs`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `password_reset_tokens`
---
-ALTER TABLE `password_reset_tokens`
-  ADD PRIMARY KEY (`email`);
 
 --
 -- Indexes for table `prodis`
@@ -622,19 +518,7 @@ ALTER TABLE `cpls`
 -- AUTO_INCREMENT for table `dosens`
 --
 ALTER TABLE `dosens`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
-
---
--- AUTO_INCREMENT for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `jobs`
---
-ALTER TABLE `jobs`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `kategori_beritas`
@@ -652,7 +536,7 @@ ALTER TABLE `mata_kuliahs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `prodis`
@@ -664,7 +548,7 @@ ALTER TABLE `prodis`
 -- AUTO_INCREMENT for table `tendiks`
 --
 ALTER TABLE `tendiks`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
